b84f7a92badf633094f3b2fb295b5fa4c205bda4
Nate Brennand <natebrennand@gmail.com>
2015-03-05 11:14:29 -0500

    add flexibility to aggregate functions

--- parallel.go
+++ parallel.go
@@ -9,6 +9,25 @@ import (
 type Manager struct {
 	wg   *sync.WaitGroup
 	errs []error
+	lock *sync.Mutex
+	agg  Aggregator
+}
+
+// Aggregator is the signature of a function used to aggregate a possible list of errors
+// into a single error to be digested by the client.
+type Aggregator func([]error) error
+
+func defaultAggregator(errs []error) error {
+	if len(errs) == 0 {
+		return nil
+	}
+
+	// accumulate all errors
+	errStr := ""
+	for _, e := range errs {
+		errStr += e.Error() + ", "
+	}
+	return fmt.Errorf("ERR: {%s}", errStr)
 }
 
 // DefaultManager creates a general manager. It fits all use cases.
@@ -16,6 +35,18 @@ func DefaultManager() Manager {
 	return Manager{
 		wg:   &sync.WaitGroup{},
 		errs: []error{},
+		lock: &sync.Mutex{},
+		agg:  defaultAggregator,
+	}
+}
+
+// CustomClient creates a manager with a provided accumulator fn.
+func CustomClient(fn Aggregator) Manager {
+	return Manager{
+		wg:   &sync.WaitGroup{},
+		errs: []error{},
+		lock: &sync.Mutex{},
+		agg:  fn,
 	}
 }
 
@@ -23,8 +54,10 @@ func DefaultManager() Manager {
 func (m *Manager) Start(f func() error) {
 	m.wg.Add(1)
 	go func() {
-		if e := f(); e != nil {
-			m.errs = append(m.errs, e)
+		if err := f(); err != nil {
+			m.lock.Lock()
+			m.errs = append(m.errs, err)
+			m.lock.Unlock()
 		}
 		m.wg.Done()
 	}()
@@ -32,17 +65,9 @@ func (m *Manager) Start(f func() error) {
 
 // Return blocks and aggregates any errors
 func (m *Manager) Return() error {
-	// block while waiting
+	// block while waiting for all fn calls to finish
 	m.wg.Wait()
 
-	// accumulate all errors needed
-	if len(m.errs) > 0 {
-		errStr := ""
-		for _, e := range m.errs {
-			errStr += e.Error()
-		}
-		return fmt.Errorf("ERR: {%s}", errStr)
-	}
-
-	return nil
+	// call accumulator to aggregate errors
+	return m.agg(m.errs)
 }
